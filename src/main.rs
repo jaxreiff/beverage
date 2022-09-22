774954b6853f6faf3b6de237498876e8a67e658d
Jax Reiff <jaxreiff@me.com>
2022-09-22 07:13:07 +0000

    replaced placeholder game with kitty sprite

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,51 @@
 // disable console on windows for release builds
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use bevy::prelude::*;
 use bevy::prelude::{App, ClearColor, Color, Msaa, NonSend, WindowDescriptor};
 use bevy::window::WindowId;
 use bevy::winit::WinitWindows;
 use bevy::DefaultPlugins;
-use bevy_game::GamePlugin;
 use std::io::Cursor;
 use winit::window::Icon;
 
 fn main() {
     App::new()
         .insert_resource(Msaa { samples: 1 })
+        .insert_resource(bevy::render::texture::ImageSettings::default_nearest())
         .insert_resource(ClearColor(Color::rgb(0.4, 0.4, 0.4)))
         .insert_resource(WindowDescriptor {
             width: 800.,
             height: 600.,
-            title: "Bevy game".to_string(), // ToDo
+            title: "beverage".to_string(),
             canvas: Some("#bevy".to_owned()),
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
-        .add_plugin(GamePlugin)
+        .add_plugin(KittyPlugin)
         .add_startup_system(set_window_icon)
         .run();
+}
+
+pub struct KittyPlugin;
+
+impl Plugin for KittyPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(kitty_setup);
+    }
+}
+
+fn kitty_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(Camera2dBundle::default());
+    commands.spawn_bundle(SpriteBundle {
+        texture: asset_server.load("textures/kitty.png"),
+        transform: Transform {
+            translation: Vec3::new(0., 0., 1.),
+            scale: Vec3::new(7., 7., 7.),
+            ..Default::default()
+        },
+        ..default()
+    });
 }
 
 // Sets the icon on windows and X11
